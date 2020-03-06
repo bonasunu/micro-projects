c10355c7644bd4d54deaf057f3a495408d546ae9
bonasunu <bonaventurasunu@gmail.com>
2020-03-06 19:17:51 +0700

    create function for chiper

--- freeCodeCamp/caesar.js
+++ freeCodeCamp/caesar.js
@@ -1,6 +1,16 @@
 function rot13(str) {
-
-    return str;
+    let decoded = [];
+    for (let item in str){
+        if (str.charCodeAt(item) > 64 && str.charCodeAt(item) < 91){
+            if ((str.charCodeAt(item) - 13) < 65){
+                decoded.push(String.fromCharCode(str.charCodeAt(item) - 13 + 26));
+            }
+            else {decoded.push(String.fromCharCode(str.charCodeAt(item) - 13))}
+        }
+        else {decoded.push(str[item])}
+    }
+    
+    console.log(decoded.join(""));
 }
 
 rot13("SERR PBQR PNZC");
