d6f64444728156cf5b21ef1c18025b667cf7b979
bonasunu <bonaventurasunu@gmail.com>
2020-03-06 20:36:05 +0700

    create function

--- freeCodeCamp/telephone.js
+++ freeCodeCamp/telephone.js
@@ -1,5 +1,44 @@
 function telephoneCheck(str) {
-    return true;
+    if (str.length === 12) {
+        if (str.match(/\d{3}-\d{3}-\d{4}/g)) {
+            return true;
+        }
+        else if (str.match(/\d{3}\s\d{3}\s\d{4}/g)) {
+            return true;
+        }
+        else return false;
+    }
+    else if (str.length === 13) {
+        if (str.match(/\(\d{3}\)\d{3}-\d{4}/g)) {
+            return true;
+        }
+        else return false;
+    }
+    else if (str.length === 14) {
+        if (str.match(/^\(\d{3}\)\s\d{3}-\d{4}/g)) {
+            return true;
+        }
+        else if (str.match(/1\s\d{3}\s\d{3}\s\d{4}/g)) {
+            return true;
+        }
+        else if (str.match(/1\s\d{3}-\d{3}-\d{4}/g)) {
+            return true;
+        }
+        else if (str.match(/^1\(\d{3}\)\d{3}-\d{4}/g)) {
+            return true;
+        }
+        else return false;
+    }
+    else if (str.length === 10) {
+        if (str.match(/\d+/g)) {
+            return true;
+        }
+        return false;
+    }
+    else if (str.match(/^1\s\(\d{3}\)\s\d{3}-\d{4}/)) {
+        return true;
+    }
+    else return false;
 }
 
 telephoneCheck("555-555-5555");
