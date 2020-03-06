cbe86d58db8cd17c5b9f9ab4a56c42146de74c55
bonasunu <bonaventurasunu@gmail.com>
2020-03-07 01:33:10 +0700

    modify algorithm for status: open

--- freeCodeCamp/cash.js
+++ freeCodeCamp/cash.js
@@ -14,12 +14,14 @@ function checkCashRegister(price, cash, cid) {
         ["TWENTY", 0],
         ["ONE HUNDRED", 0]
     ];
+    let totalNewCid = 0;
+    let open = {status: "OPEN", change: []};
 
     // Recursion function
     function money(change, usd, currency){
         change -= usd;
         newCid[currency][1] += usd; 
-        if (change >= usd){
+        if (change >= usd && newCid[currency][1] < cid[currency][1]){
             money(change, usd, currency);
         }
     }
@@ -72,12 +74,19 @@ function checkCashRegister(price, cash, cid) {
 
     }
 
-    console.log(totalCid);
-    console.log(change);
+    // Algorithm for changeand status: open
+    for (let k = 0; k < newCid.length; k++){
+        totalNewCid += newCid[k][1];
+    };
+
+    if (totalNewCid != 0){
+        console.log(totalCid);
+        console.log(change);
+        
+    }
+
     console.log(newCid);
-    console.log(newCid[8][1]);
-    console.log(newCid[8][1] += 100);
     
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
