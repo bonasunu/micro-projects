4296890561a0ec7d080700e6158533d1470559c6
bonasunu <bonaventurasunu@gmail.com>
2020-03-09 10:58:58 +0700

    modify first function

--- eloquent-js/reverseArray.js
+++ eloquent-js/reverseArray.js
@@ -1,15 +1,20 @@
 function reverseArray(arr){
-    console.log(arr);
+    let newArr = [];
+    for (let item in arr){
+        newArr.unshift(arr[item]);
+    }
+    return newArr;
 }
 
-reverseArray([1, 2, 3, 4, 5]);
+console.log(reverseArray([1, 2, 3, 4, 5]));
 
 function reverseArrayInPlace(arr){
     let newArr = [];
     for (let item in arr){
         newArr.unshift(arr[item]);
     }
-    console.log(newArr);
+    return newArr;
 }
 
 reverseArrayInPlace([1, 2, 3, 4, 5]);
+
