7e13fd3a7ad2611f602bc1d22357415f2ca1c149
bonasunu <bonaventurasunu@gmail.com>
2020-03-09 09:00:05 +0700

    create function for sum numbers

---
+++ eloquent-js/sum.js
@@ -0,0 +1,10 @@
+// Function takes 2 arguments and returns the sum of the numbers between num1 and num2
+function sumArray(num1, num2){
+    let total = 0;
+    for (let i = num1; i <= num2; i++){
+        total += i;
+    }
+    console.log(total);
+}
+
+sumArray(1, 10);
