3a4a4e2f16415c5eed3e877d96bc1aae56a41ac8
bonasunu <bonaventurasunu@gmail.com>
2020-03-09 09:10:10 +0700

    create stepArray function

--- eloquent-js/sum.js
+++ eloquent-js/sum.js
@@ -10,3 +10,24 @@ function sumArray(num1, num2){
 sumArray(1, 10);
 
 // Function that takes 3 arguments and returns array. 
+function stepArray(start, end, step = 1){
+    let arr = [];
+    if (step > 0){
+        for (let i = start; i <= end; i += step) {
+            arr.push(i);
+        }
+    }
+    else if (step < 0){
+        for (let i = start; i >= end; i += step) {
+            arr.push(i);
+        }
+    }
+    else {
+        for (let i = start; i <= end; i++) {
+            arr.push(i);
+        }
+    }
+    console.log(arr);
+}
+
+stepArray(5, 2, -1);
