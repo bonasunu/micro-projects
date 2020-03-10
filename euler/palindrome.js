1c18841104c0992425b397f94a7810ca6470e3a3
bonasunu <bonaventurasunu@gmail.com>
2020-03-10 17:29:08 +0700

    edit function

--- euler/palindrome.js
+++ euler/palindrome.js
@@ -1,8 +1,18 @@
-for (let i = 900; i < 1000; i++){
-    for (let j = 900; j < 1000; j++){
-        let num = i * j;
-        if (num === 906609){
-            console.log(i, j);
+function largestPalindromeProduct(n) {
+    let num = Math.pow((Math.pow(10, n) - 1), 2);
+    let reverseNum = "";
+    for (let i = num; i < 0; i--) {
+
+        for (let j = i.toString().length - 1; j >= 0; j--) {
+            reverseNum += i.toString()[j];
+        }
+        if (num == reverseNum) {
+            console.log(parseInt(num));
+            break;
         }
     }
+    
+    console.log(reverseNum);
 }
+
+largestPalindromeProduct(2);
