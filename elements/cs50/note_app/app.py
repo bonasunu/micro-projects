e4008a1824006e172a2b98d3e69f9cdcfe2b85fb
bonasunu <bonaventurasunu@gmail.com>
2020-03-23 10:49:39 +0700

    add app route

--- elements/cs50/note_app/app.py
+++ elements/cs50/note_app/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, session
 from flask_session import session
 
+app = Flask(__name__)
+
 app.config["SESSION_PERMANENT"] = False 
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.methods == "POST"
+        note = request.form.get("note")
+        sessionp["notes"].append(note)
+
+    render_template(index.html, notes=session["notes"])
