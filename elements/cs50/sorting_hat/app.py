30132b4c8610dcec993353898b653b0cd7144742
bonasunu <bonaventurasunu@gmail.com>
2020-03-27 23:08:07 +0700

    add gryffindor function

--- elements/cs50/sorting_hat/app.py
+++ elements/cs50/sorting_hat/app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/gryffindor")
+def gryffindor():
+    return render_template("gryffindor.html")
+
+@app.route("/planes", methods=['GET'])
+def plane():
+    flights = db.execute("SELECT origin, destination, duration FROM flights").fetchall()
+    for flight in flights:
+        print(f"{flight.origin} to {flight.destination}, {flight.duration} minutes.")
+
+if __name__ == '__main __':
+    app.run(debug=True)
