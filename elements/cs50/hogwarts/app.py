f928784494c99d5a00a6370da2ce58161dea80f3
bonasunu <bonaventurasunu@gmail.com>
2020-03-28 21:37:11 +0700

    add randintfor houses

--- elements/cs50/hogwarts/app.py
+++ elements/cs50/hogwarts/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
+@app.route("/index")
 @app.route("/")
 def index():
+    house = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff']
+    num = random.randint(0,3)
+    new_house = house[num]
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
     return render_template('index.html')
 
 if (__name__) == '__main__':
