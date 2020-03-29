d3877eb8b12506db21be162f0ec8dc35fcce2208
bonasunu <bonaventurasunu@gmail.com>
2020-03-29 23:18:21 +0700

    add main() function

--- elements/cs50/class1.py
+++ elements/cs50/class1.py
@@ -9,3 +9,7 @@ class Flight:
         print(f"Flight origin: {self.origin}")
         print(f"Flight destination: {self.destination}")
         print(f"Flight duration: {self.duration}")
+
+def main():
+    f1 = Flight('New York', 'Paris', 540)
+    f1.print_info()
