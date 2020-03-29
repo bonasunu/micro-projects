ccef30e5faf697a1591c045a76152a11dc731226
bonasunu <bonaventurasunu@gmail.com>
2020-03-29 23:17:14 +0700

    define class Flight with new property - print_info

---
+++ elements/cs50/class1.py
@@ -0,0 +1,11 @@
+class Flight:
+
+    def __init__(self, origin, destination, duration):
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+
+    def print_info(self):
+        print(f"Flight origin: {self.origin}")
+        print(f"Flight destination: {self.destination}")
+        print(f"Flight duration: {self.duration}")
