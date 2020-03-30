9e1136d938e430e1b5ec2d741585e5ecf480d5ed
bonasunu <bonaventurasunu@gmail.com>
2020-03-30 13:45:49 +0700

    add full app.py

--- elements/cs50/airline/app.py
+++ elements/cs50/airline/app.py
@@ -9,5 +9,41 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    
+    flights = Flight.query.all()
+    return render_template("index.html", flights=flights)
 
+@app.route('/book', methods=['POST'])
+def book():
+    name = request.form.get('name')
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number")
+
+    # Make sure the flight exists
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return render_template("error.html", message="No such flight with that id")
+
+    # Add passenger
+    passenger = Passenger(name=name, flight_id=flight_id)
+    db.session.add(passenger)
+    db.session.commit()
+    return render_template("sucess.html")
+
+@app.route('/flights')
+def flights():
+    """List all flights"""
+    flights = Flight.query.all()
+    return render_template("flights.html", flights=flights)
+
+@app.route('/flights/<int:flight_id>')
+def flight(flight_id):
+    # Make sure flight exist
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return render_template("error.html", message="No such flight")
+
+    # Get all passengers
+    passengers = Passenger.query.filter_by(flight_id=flight_id).all()
+    return render_template("flight.html", flight=flight, passengers=passengers)
