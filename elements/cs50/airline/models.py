15b1c88ec12c4a1ffe358948269e9f31d93c3b19
bonasunu <bonaventurasunu@gmail.com>
2020-03-30 13:19:27 +0700

    code models

---
+++ elements/cs50/airline/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
+
+class Flight(db.model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+class Passenger(db.model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flight_id"), nullable=False)
