292db3bcce9b17167f4f265cc4971714de6254dc
bonasunu <bonaventurasunu@gmail.com>
2020-03-31 11:43:53 +0700

    add db command to create database

--- elements/cs50/member-orm/app.py
+++ elements/cs50/member-orm/app.py
@@ -7,7 +7,17 @@ app.config['SQL_ALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+def main():
+    db.Create_all()
+
 @app.route('/index')
 @app.route('/')
 def index():
-    
+    members = Member('Nadia', 'Pucci')
+    db.session.add(members)
+    db.session.commit()
+    return render_template('index.html')
+
+if __name__='__main__':
+    with app.app_contect():
+        main()
