b9532e83bc93cae90864d00700ac0f34e9bbe2c5
bonasunu <bonaventurasunu@gmail.com>
2020-04-02 17:42:40 +0700

    implement login and register using wtforms

--- capstone/loginsys/app.py
+++ capstone/loginsys/app.py
@@ -1,8 +1,24 @@
 import os
 
 from flask import Flask, session, redirect, url_for, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissupposedtobesecretkey'
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    fullname = StringField('fullname', validators=[InputRequired(), Length(min=1, max=15)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route("/")
 def index():
@@ -12,15 +28,24 @@ def index():
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        return 'Login Success'
+    return render_template("login.html", form=form)
 
 @app.route("/logout")
 def logout():
     return redirect("/")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return 'register Success'
+    return render_template("register.html", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
