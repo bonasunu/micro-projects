34a727710c7fa5f8e1fe2a1b890525da25a0c9f1
bonasunu <bonaventurasunu@gmail.com>
2020-04-04 21:19:47 +0700

    add import request

--- elements/api.py
+++ elements/api.py
@@ -1,4 +1,4 @@
-import os
+import requests
 
 res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "UnA8Pqrbqg98bh2gRA", "isbns": "9781632168146"})
 print(res.json())
