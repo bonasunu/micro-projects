031ce7a220fc39434f4e7e19b1e32bcb98329477
bonasunu <bonaventurasunu@gmail.com>
2020-04-08 21:42:27 +0700

    create routes on app.py

---
+++ elements/cs50/ajax-currency/app.py
@@ -0,0 +1,28 @@
+import requests
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    
+    # Query for currency exchange rate
+    currency = request.form.get('currency')
+    res = requests.get("http://data.fixer.io/api/latest",
+                     params={"base": "USD", "symbols": currency})
+
+    # Make sure request succeeded
+    if res.status_code != 200:
+        return jsonify({"success": False})
+
+    data =res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"]["currency"]})    
+    
