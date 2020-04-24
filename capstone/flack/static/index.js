a42716c0cd471f5640e4e5bf1c15a0b96db9bb17
bonasunu <bonaventurasunu@gmail.com>
2020-04-24 22:42:20 +0700

    add h2 element when socket connected

--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -5,11 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Check if user exist when connected. Otherwise, call function to create it
     socket.on('connect', () => {
-        
+
+        // To do - Implement Local Storage
+
         const h2 = document.createElement('h2');
-        h2.innerHTML = "Hello, user";
+        h2.innerHTML = "Hello!";
         document.querySelector('#hello').append(h2);
-        
+
     });
 
 });
