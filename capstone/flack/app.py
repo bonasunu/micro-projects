67ff8de7778ef95a53607b7036af6265fac27dd3
bonasunu <bonaventurasunu@gmail.com>
2020-04-25 18:33:43 +0700

    socketio is working. hell yeah. start building other features

--- capstone/flack/app.py
+++ capstone/flack/app.py
@@ -10,8 +10,6 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-#add socket on
-
+    
 if __name__ == '__main__':
     socketio.run(app)
--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -7,9 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     // Check if user exist when connected. Otherwise, call function to create it
     socket.on('connect', () => {
 
-        let h2 = document.createElement('h2');
-        h2.innerHTML = 'Hello Bonaventura';
-        document.querySelector('#hello').append(h2);
+        document.querySelector('#hello').append('Connected');
+        if (!localStorage.getItem('username')) {
+            localStorage.setItem('username', 'Bonaventura');
+        };
 
+        const h2 = document.createElement('h2');
+        h2.innerHTML = `Hello ${localStorage.getItem('username')}!`;
+        document.querySelector('#hello').append(h2);
     });
+
+    // Delete data
+    document.querySelector('button').addEventListener('click', () => {
+        localStorage.clear();
+        while (document.querySelector('#hello').firstChild) {
+            document.querySelector('#hello').removeChild(document.querySelector('#hello').firstChild);
+        };
+    });
+
 });
+
+
