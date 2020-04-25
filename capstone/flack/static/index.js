9f847404fd10b8f53d4cd90bbe6e756115c4c2c8
bonasunu <bonaventurasunu@gmail.com>
2020-04-25 13:41:13 +0700

    edit localstorage script

--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -1,19 +1,35 @@
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    createUser();
+    function createUser() {
+
+        // To do - Implement Local Storage
+        if (!localStorage.getItem('user')) {
+    
+            var userInput = prompt("Input your username")
+            localStorage.setItem('user', userInput);
+            
+        };
+    };
+    
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     // Check if user exist when connected. Otherwise, call function to create it
     socket.on('connect', () => {
 
-        // To do - Implement Local Storage
-
         const h2 = document.createElement('h2');
-        h2.innerHTML = "Hello!";
+        h2.innerHTML = `Hello ${userInput}!`;
         document.querySelector('#hello').append(h2);
+        document.Elem
 
     });
 
-    document.querySelector('#deleteData').onClick = () => {localStorage.clear()};
-
+    document.querySelector('#deleteData').onClick = () => {
+        localStorage.clear();
+        const h2 = document.querySelector('h2');
+        h2.remove();
+        createUser();
+    };
 });
