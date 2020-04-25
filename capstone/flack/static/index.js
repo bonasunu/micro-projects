f35d5dfec0fa3e8be36a85633caf256416f5a62a
bonasunu <bonaventurasunu@gmail.com>
2020-04-25 17:47:13 +0700

    socketio not working properly. fixing it

--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -1,19 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    createUser();
-    function createUser() {
-
-        // To do - Implement Local Storage
-
-        // Jsonify
-        if (!localStorage.getItem('user')) {
-    
-            var userInput = prompt("Input your username")
-            localStorage.setItem('user', userInput);
-            
-        };
-    };
     
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -21,17 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Check if user exist when connected. Otherwise, call function to create it
     socket.on('connect', () => {
 
-        const h2 = document.createElement('h2');
-        h2.innerHTML = `Hello ${userInput}!`;
+        let h2 = document.createElement('h2');
+        h2.innerHTML = 'Hello Bonaventura';
         document.querySelector('#hello').append(h2);
-        document.Elem
 
     });
-
-    document.querySelector('#deleteData').onClick = () => {
-        localStorage.clear();
-        const h2 = document.querySelector('h2');
-        h2.remove();
-        createUser();
-    };
 });
