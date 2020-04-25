3e8d1e7e1a5c1377fdee7746cfb7822b7378dc41
bonasunu <bonaventurasunu@gmail.com>
2020-04-25 22:25:13 +0700

    creat socketio to add new channel

--- capstone/flack/app.py
+++ capstone/flack/app.py
@@ -7,9 +7,17 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Channels list using python list datatype
+channels = []
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("add channel")
+def addChannel(data):
+    channelName = data["channelName"]
+    channels.append(channelName)
     
 if __name__ == '__main__':
     socketio.run(app)
