37c3ef43235c27e68acd4405a5d0bdea8b631b7f
bonasunu <bonaventurasunu@gmail.com>
2020-04-29 23:32:04 +0700

    edit file to try socketio connection

--- capstone/flack/app.py
+++ capstone/flack/app.py
@@ -1,23 +1,23 @@
 import os
-import requests
 
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, url_for, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-# Channels list using python list datatype
-channelList = []
+myList = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", channelList=channelList)
+    return render_template('index.html', myList=myList)
 
-@socketio.on("add channel")
-def add_channel(data):
-    channelList.append(data["chName"])
-    
+@socketio.on("user connected")
+def connected(data):
+    ch = data["connected"]
+    myList.append(ch)
+    emit('myList', myList, broadcast=True)
+
 if __name__ == '__main__':
     socketio.run(app)
