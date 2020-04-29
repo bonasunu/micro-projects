3f2668ba20cf0b1e088dfc9fc380548c826112bc
bonasunu <bonaventurasunu@gmail.com>
2020-04-30 00:09:51 +0700

    edit file to try socketio connection

--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -1,44 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // Check if user exist when connected. Otherwise, call function to create it
+    // When connected, send info
     socket.on('connect', () => {
 
-        if (!localStorage.getItem('username')) {
-            document.querySelector('#form').style.visibility = 'visible';
-            document.querySelector('#form').addEventListener('submit', () => {
-                let user = document.querySelector('#userInput').value;
-                localStorage.setItem('username', user);
-            });
-            
-        }
-        else {
-            document.querySelector('#form').style.visibility = 'hidden';
-            const h2 = document.createElement('h2');
-            h2Value = localStorage.getItem('username');
-            h2.innerHTML = 'Hello ' + h2Value;
-            document.querySelector('#hello').append(h2);
-        };
-
-        // Create channel and add to channelList in app.py via "add channel" event
-        document.querySelector('#createChannel').addEventListener('submit', () => {
-            let chName = document.querySelector('#channelName').value;
-            socket.emit('add channel', {'chName': chName});
-        });
-
+        socket.emit('user connected', {"connected": "SocketIO is up and running"});
     });
 
-    // Delete data on localStorage
-    document.querySelector('button').addEventListener('click', () => {
-        localStorage.clear();
-        while (document.querySelector('#hello').firstChild) {
-            document.querySelector('#hello').removeChild(document.querySelector('#hello').firstChild);
-        };
-    });
+    socket.on('myList', data => {
+        let h2 = document.createElement("h2");
+        h2.innerHTML = data;
+        document.querySelector("body").append(h2);
 
+    });
 });
-
-
