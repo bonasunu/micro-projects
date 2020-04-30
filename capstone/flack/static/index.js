1b6acaeb0f3007287a8569f76820a611e462b895
bonasunu <bonaventurasunu@gmail.com>
2020-04-30 15:22:07 +0700

    edit for channel creation

--- capstone/flack/static/index.js
+++ capstone/flack/static/index.js
@@ -7,17 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     // When connected, send info
     socket.on('connect', () => {
         let user = "Bona";
-
+        
         socket.emit('user connected', {"user": user});
     });
 
+    // Send greeting message to user
     socket.on('greeting', data => {
         let h2 = document.createElement("h2");
         h2.innerHTML = "Hello " + data;
         document.querySelector("#greet").append(h2);
     });
 
-
+    // Create channel event
     document.querySelector("#create_channel").onsubmit = () => {
         // Emit the channel creation event using the input from the user
         const channel = document.querySelector("#channel").value;
@@ -26,9 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     };
 
+    // Show list of channels
     socket.on('channel list', data => {
-        let h3 = document.createElement("h3");
-        h3.innerHTML = data;
-        document.querySelector("body").append(h3);
+        let li = document.createElement('a');
+        li.innerHTML = '# ' + data;
+        document.querySelector("#ch_list").append(li);
+        
     });
 }); 
