f683a7faa25f4cc10fa9f4418c8f720a28b2225e
bonasunu <bonaventurasunu@gmail.com>
2020-05-08 11:11:34 +0700

    edit pizza models.py

--- capstone/pino/pizza/models.py
+++ capstone/pino/pizza/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class RegPizza(models.Model):
-    # Pizza size
-    PIZZA_SIZES = (
-        ('S', 'Small'),
-        ('L', 'Large')
-    )
-    topping = models.CharField(max_length=64)
-    size = models.CharField(max_length=1, choices=PIZZA_SIZES)
+
+class Pasta(models.Model):
+    # Model
+    menu = models.CharField(max_length=64)
     price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.menu} - {self.price}"
