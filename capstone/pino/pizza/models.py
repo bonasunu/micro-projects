499825276ff5d9c9da2b7b86a4df26156f82a1d2
bonasunu <bonaventurasunu@gmail.com>
2020-05-08 12:22:12 +0700

    edit pizza models

--- capstone/pino/pizza/models.py
+++ capstone/pino/pizza/models.py
@@ -9,3 +9,23 @@ class Pasta(models.Model):
 
     def __str__(self):
         return f"{self.menu} - {self.price}"
+
+class Salads(models.Model):
+    salad = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.salad} - {self.price}"
+
+class Platters(models.Model):
+    SALAD_SIZE = (
+        ("S", "Small"),
+        ("L", "Large")
+    )
+
+    platter = models.CharField(max_length=64)
+    size = models.CharField(max_length=1, choices=SALAD_SIZE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.platter} - {self.size} - {self.price}"
