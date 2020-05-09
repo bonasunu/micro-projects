2b4f4c7843735a06eb2ca4b68017b80a7c4f2328
bonasunu <bonaventurasunu@gmail.com>
2020-05-09 23:35:44 +0700

    create class Tests to testis_prime

---
+++ elements/cs50/unittest0.py
@@ -0,0 +1,17 @@
+import unittest
+
+from prime import is_prime
+
+class Tests(unittest.TestCase):
+
+    def test_1(self):
+        self.assertFalse(is_prime(1))
+
+    def test_2(self):
+        self.assertFalse(is_prime(8))
+
+    def test_3(self):
+        self.assertFalse(is_prime(2))
+
+if __name__ == "__main__":
+    unittest.main()
