301b007c1cf204bd62bd0d21b295aac35c6f9376
bonasunu <bonaventurasunu@gmail.com>
2020-05-10 15:19:50 +0700

    create simple test inside flight app - djangoair

--- elements/cs50/djangoair/flights/tests.py
+++ elements/cs50/djangoair/flights/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from flights.models import Airport, Flight
 
 # Create your tests here.
+def set_up(self):
+
+    a1 = Airport.objects.create(code="AAA", city="City A")
+    a2 = Airport.objects.create(code="BBB", city="City B")
+
+    # Create flight
+    Flight.objects.create(origin=a1, destination=a2, duration=100)
+    Flight.objects.create(origin=a1, destination=a1, duration=200)
+    Flight.objects.create(origin=a1, destination=a2, duration=-100)
+
+def test_departures_count(self):
+    a = Airport.objects.get(code="AAA")
+    self.assertEqual(a.departures.count(), 3)
