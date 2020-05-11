6876a20205f38c112eab55793c66012e19d712e0
bonasunu <bonaventurasunu@gmail.com>
2020-05-11 21:37:05 +0700

    add class for front end test inside djangoair

--- elements/cs50/djangoair/flights/tests.py
+++ elements/cs50/djangoair/flights/tests.py
@@ -1,28 +1,38 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from flights.models import Airport, Flight
 
 # Create your tests here.
-def set_up(self):
+class ModelsTestCase(TestCase):
 
-    a1 = Airport.objects.create(code="AAA", city="City A")
-    a2 = Airport.objects.create(code="BBB", city="City B")
+    def setUp(self):
 
-    # Create flight
-    Flight.objects.create(origin=a1, destination=a2, duration=100)
-    Flight.objects.create(origin=a1, destination=a1, duration=200)
-    Flight.objects.create(origin=a1, destination=a2, duration=-100)
+        a1 = Airport.objects.create(code="AAA", city="City A")
+        a2 = Airport.objects.create(code="BBB", city="City B")
 
-def test_departures_count(self):
-    a = Airport.objects.get(code="AAA")
-    self.assertEqual(a.departures.count(), 3)
+        # Create flight
+        Flight.objects.create(origin=a1, destination=a2, duration=100)
+        # Flight.objects.create(origin=a1, destination=a1, duration=200)
+        # Flight.objects.create(origin=a1, destination=a2, duration=-100)
 
-def test_arrivals_count(self):
-    a = Airport.objects.get(code="AAA")
-    self.assertEqual(a.arrivals.count(), 1)
+    def test_departures_count(self):
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.departures.count(), 3)
 
-def test_valid_flight(self):
-    a1 = Airport.objects.get(code="AAA")
-    a2 = Airport.objects.get(code="BBB")
+    def test_arrivals_count(self):
+        a = Airport.objects.get(code="AAA")
+        self.assertEqual(a.arrivals.count(), 1)
 
-    f = Flight.objects.get(origin=a1, destination=a2, duration=100)
-    self.assertTrue(f.is_valid_flight())
+    def test_valid_flight(self):
+        a1 = Airport.objects.get(code="AAA")
+        a2 = Airport.objects.get(code="BBB")
+
+        f = Flight.objects.get(origin=a1, destination=a2, duration=100)
+        self.assertTrue(f.is_valid_flight())
+
+class FlightsTestCase(TestCase):
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["flights"].count(), 2)
