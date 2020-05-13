9ff10ae0f41dfe18e305f93ba77b6a3cbf2cf0c2
bonasunu <bonaventurasunu@gmail.com>
2020-05-13 21:55:55 +0700

    add models to admin page

--- capstone/pino/pizza/admin.py
+++ capstone/pino/pizza/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pasta, Salads, Platters, Pizza, Toppings
 
 # Register your models here.
+admin.site.register(Pasta)
+admin.site.register(Salads)
+admin.site.register(Platters)
+admin.site.register(Pizza)
+admin.site.register(Toppings)
