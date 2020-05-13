3b14488bae95dac4dfd30a23ddb87ea28023ec59
bonasunu <bonaventurasunu@gmail.com>
2020-05-13 22:21:39 +0700

    testing inormation pass process to client using jinja

--- capstone/pino/pizza/views.py
+++ capstone/pino/pizza/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 # Create your views here.
 def index(request):
     return render(request, "pizza/index.html")
 
 def menu(request):
-    return render(request, "pizza/menu.html")
+    context = {
+        "pizza": Pizza.objects.all()
+    }
+    return render(request, "pizza/menu.html", context)
 
 def order(request):
     return render(request, "pizza/order.html")
