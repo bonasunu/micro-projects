482b1d7f08da936185f4fd4b87ffc2d8eb2a47db
bonasunu <bonaventurasunu@gmail.com>
2020-05-13 20:26:51 +0700

    create template base.html

--- capstone/pino/pizza/views.py
+++ capstone/pino/pizza/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is Pizza!")
+
+def menu(request):
+    return HttpResponse("This is menu")
