dd43c1186f53f4b32b05296875070e14f9f2aade
bonasunu <bonaventurasunu@gmail.com>
2020-05-23 02:41:13 +0700

    create cards, learn, register, and login pages

--- capstone/pijar/mooc/urls.py
+++ capstone/pijar/mooc/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.user_login, name='login'),
+    path('register', views.register_user, name='register'),
+    path('cards', views.cards, name='cards'),
+    path('learn', views.learn, name='learn'),
 ]
--- capstone/pijar/mooc/views.py
+++ capstone/pijar/mooc/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mooc/index.html')
+
+def cards(request):
+    return render(request, 'mooc/cards.html')
+
+def learn(request):
+    return render(request, 'mooc/learn.html')
+
+def register_user(request):
+    return render(request, 'mooc/register.html')
+
+def user_login(request):
+    return render(request, 'mooc/login.html')
