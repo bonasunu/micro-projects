e90e4c51a74dd81ae94ddb6c979cc309d718831a
bonasunu <bonaventurasunu@gmail.com>
2020-05-23 03:22:18 +0700

    add todo

--- capstone/pijar/mooc/models.py
+++ capstone/pijar/mooc/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+TODO
+Pijar - minimalistic flashcards to help your learning process
+no distractions
+
+Create user model
+- Full name
+- username
+- email
+- password
+
+Create cards model
+- category
+- add or delete category
+- add or remove cards inside category
+- viewing all categories
+- viewing all cards inside category
+
+Create learn model
+- select category
+- start/stop learning
+- random cards
+- load card
+
+UX on learn page
+- select category using dropdown
+- load card  to start learning (show a card)
+- stop to stop learning (clear view and show meaage info)
+- message info (how to start/stop learning)
+
+additional feature
+- save last open cards, category, or close card
+
+
+"""
