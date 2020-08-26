5bc4f8056e4fa05d83407846596322c8eaa8ed71
bonasunu <bonaventurasunu@gmail.com>
2020-08-26 23:17:39 +0700

    solve test 1

---
+++ job/5-jack.js
@@ -0,0 +1,36 @@
+const solution = (record) => {
+  let nickname = {}
+
+  const newRecord = record.map((item) => {
+    let newItem = item.split(' ')
+    let arr = [newItem[1], newItem[0]]
+    newItem[0] !== 'Leave' ? (nickname[newItem[1]] = newItem[2]) : ''
+    return arr
+  })
+
+  const answer = newRecord
+    .map((item) => {
+      switch (item[1]) {
+        case 'Enter':
+          return `${nickname[item[0]]} came in`
+        case 'Leave':
+          return `${nickname[item[0]]} has left`
+        case 'Change':
+          return 'Change'
+      }
+    })
+    .filter((item) => item !== 'Change')
+
+  console.log(answer)
+}
+
+let input = [
+  'Enter uid1234 Muzi',
+  'Enter uid4567 Prodo',
+  'Leave uid1234',
+  'Enter uid1234 Prodo',
+  'Change uid4567 Ryan',
+]
+// output ["Prodo came in.", "Ryan came in.", "Prodo has left.", "Prodo came in."]
+
+solution(input)
